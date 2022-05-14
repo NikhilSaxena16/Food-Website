dcf920edafba4ce1f09cb8909b4c862e0a3ec92b
Nikhil Saxena <87313198+NikhilSaxena16@users.noreply.github.com>
2022-05-14 17:32:37 +0530

    Add files via upload

---
+++ Your-Kitchen.js
@@ -0,0 +1,23 @@
+var menu = document.querySelector('#menu-bar');
+var navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+	menu.classList.toggle('fa-times');
+	navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+	menu.classList.remove('fa-times');
+	navbar.classList.remove('active');
+}
+
+/*Order*/
+
+function order(){
+	alert("your order is successful");
+}
+
+/*RESERVATION*/
+
+function reservation(){
+	alert("your reservation is successful");
+}
